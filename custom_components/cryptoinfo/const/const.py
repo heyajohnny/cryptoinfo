a9496d8b7f1947d6e54e0b4c25e0f9beed488fea
Martin Myhrman <martin@myhrmans.com>
2020-08-17 12:28:19 +0200

    Add 24h volume and change from Coingecko API

--- custom_components/cryptoinfo/const/const.py
+++ custom_components/cryptoinfo/const/const.py
@@ -3,9 +3,13 @@ import logging
 CONF_CRYPTOCURRENCY_NAME = "cryptocurrency_name"
 CONF_CURRENCY_NAME = "currency_name"
 CONF_UPDATE_FREQUENCY = "update_frequency"
+CONF_INCLUDE_24H_VOL = "include_24h_vol"
+CONF_INCLUDE_24H_CHANGE = "include_24h_change"
 
 SENSOR_PREFIX = "Cryptoinfo "
 ATTR_LAST_UPDATE = "last_update"
+ATTR_24H_CHANGE = "24h_change"
+ATTR_24H_VOL = "24h_vol"
 
 API_ENDPOINT = "https://api.coingecko.com/api/v3/"
 
