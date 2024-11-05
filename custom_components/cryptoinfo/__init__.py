d09681bc9ba88b63f342fcecaae21b696cf48578
heyajohnny <heyajohnny@gmail.com>
2024-11-05 12:11:49 +0100

    Use shared property for min_time_between_requests

--- custom_components/cryptoinfo/__init__.py
+++ custom_components/cryptoinfo/__init__.py
@@ -1,15 +1,16 @@
-from .const.const import _LOGGER
+from custom_components.cryptoinfo.config_flow import CryptoInfoData
+from .const.const import _LOGGER, DOMAIN
 
-from homeassistant import config_entries
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
 
 PLATFORMS = [Platform.SENSOR]
 
 
-async def async_setup_entry(
-    hass: HomeAssistant, entry: config_entries.ConfigEntry
-) -> bool:
+async def async_setup_entry(hass, entry):
+    """Set up the CryptoInfo platform."""
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = CryptoInfoData()
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     _LOGGER.debug("__init__ set up")
     return True
